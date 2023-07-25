24cceb29fb5ce3a7a70b32009bb52a98c6474fda
yuelun <yuelun@amazon.com>
2023-07-25 22:53:13 +0800

    adding constructor for model: Article

--- src/main/java/com/tencent/wxcloudrun/model/Article.java
+++ src/main/java/com/tencent/wxcloudrun/model/Article.java
@@ -1,10 +1,10 @@
 package com.tencent.wxcloudrun.model;
 
 import lombok.Builder;
-
-import java.util.List;
+import lombok.Data;
 
 @Builder
+@Data
 public class Article {
     private String articleId;
     private String title;
