21fc98452b17ea0b0862a151897d366fad16fdf2
yuelun <yuelun@amazon.com>
2023-08-19 17:53:35 +0800

    adding more details to Article

--- src/main/java/com/tencent/wxcloudrun/dto/article/CreateArticleRequest.java
+++ src/main/java/com/tencent/wxcloudrun/dto/article/CreateArticleRequest.java
@@ -2,7 +2,29 @@ package com.tencent.wxcloudrun.dto.article;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Data
 public class CreateArticleRequest {
     private String title;
+    private Integer articleType;
+    private String merchantCategory;
+    private List<String> images;
+    private String content; // default empty
+    private Double price;
+    private Boolean isDeliveryCovered;
+    private Integer deliveryMethod;
+    private String location;
+    private Boolean isNeedHelp;
+    private Long createTimestamp;
+    private Long updateTimestamp;
+    private String authorName;
+    private String authorId;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private Long likeCount;
+    private Long viewCount;
+    private Long colCount;
+    private Boolean isStickTop;
 }
--- src/main/java/com/tencent/wxcloudrun/model/Article.java
+++ src/main/java/com/tencent/wxcloudrun/model/Article.java
@@ -3,9 +3,35 @@ package com.tencent.wxcloudrun.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Builder
 @Data
 public class Article {
     private String articleId;
     private String title;
+    private int articleType;
+    private String merchantCategory;
+    private List<String> images;
+    private String content; // default empty
+    private Double price;
+    private boolean isDeliveryCovered;
+    private int deliveryMethod;
+    private String location;
+
+    private boolean isNeedHelp;
+
+    private Long createTimestamp;
+    private Long updateTimestamp;
+    private String authorName;
+    private String authorId;
+
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    private Long likeCount;
+    private Long viewCount;
+    private Long colCount;
+    private boolean isStickTop;
 }
