0642a0d1764b543fa956c126c5e54f90d5e90f8b
yuelun <yuelun@amazon.com>
2023-10-08 21:50:05 +0800

    adding builder for create user request

--- src/main/java/com/tencent/wxcloudrun/dto/user/CreateUserRequest.java
+++ src/main/java/com/tencent/wxcloudrun/dto/user/CreateUserRequest.java
@@ -1,8 +1,10 @@
 package com.tencent.wxcloudrun.dto.user;
 
 import com.tencent.wxcloudrun.model.User;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class CreateUserRequest extends User {
 }
