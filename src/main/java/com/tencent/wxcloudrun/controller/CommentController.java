04881da35c58dc70a3086ec42e1f248812a8ea8f
yuelun <yuelun@amazon.com>
2023-10-18 21:52:46 +0800

    fix a issue

--- src/main/java/com/tencent/wxcloudrun/controller/CommentController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CommentController.java
@@ -1,11 +1,9 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
-import com.tencent.wxcloudrun.dto.article.CreateArticleRequest;
 import com.tencent.wxcloudrun.dto.comment.CreateCommentRequest;
 import com.tencent.wxcloudrun.model.ArticleComment;
 import com.tencent.wxcloudrun.service.ArticleCommentService;
-import com.tencent.wxcloudrun.service.ArticleSerice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -34,7 +33,7 @@ public class CommentController {
         return ApiResponse.ok(commentId);
     }
 
-    @PostMapping(value = "/api/article-comment-management/get-all-comments-for-article/{articleId}")
+    @GetMapping(value = "/api/article-comment-management/get-all-comments-for-article/{articleId}")
     ApiResponse getAllCommentsForArticle(@PathVariable String articleId) {
         logger.info("getAllCommentsForArticle article id: {}", articleId);
         final List<ArticleComment> comments = articleCommentService.getAllCommentsForArticle(articleId);
