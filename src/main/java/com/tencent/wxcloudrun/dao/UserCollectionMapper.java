77d7879a62e671fc9e88d83d5e4fc8d52a7b7b99
yuelun <yuelun@amazon.com>
2023-11-11 16:20:41 +0800

    fix a issue

--- src/main/java/com/tencent/wxcloudrun/dao/UserCollectionMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/UserCollectionMapper.java
@@ -1,6 +1,5 @@
 package com.tencent.wxcloudrun.dao;
 
-import com.tencent.wxcloudrun.model.FollowPair;
 import com.tencent.wxcloudrun.model.UserCollection;
 import org.apache.ibatis.annotations.Mapper;
 
