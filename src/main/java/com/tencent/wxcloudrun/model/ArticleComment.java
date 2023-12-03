296426c3d6ee2f302cbb28b1b3f95e4cb26ea6c2
yuelun <yuelun@amazon.com>
2023-12-03 21:17:28 +0800

    fix out of bound

--- src/main/java/com/tencent/wxcloudrun/model/ArticleComment.java
+++ src/main/java/com/tencent/wxcloudrun/model/ArticleComment.java
@@ -1,13 +1,17 @@
 package com.tencent.wxcloudrun.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleComment {
     private String commentId;
     private String content;
