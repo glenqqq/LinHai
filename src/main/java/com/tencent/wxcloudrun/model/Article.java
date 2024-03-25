9e9fdfa6b2371ab5e628c760a8b8cfc9a4606ba9
yuelun <yuelun@amazon.com>
2024-03-25 14:37:57 +0800

    adding no arg constructor

--- src/main/java/com/tencent/wxcloudrun/model/Article.java
+++ src/main/java/com/tencent/wxcloudrun/model/Article.java
@@ -1,13 +1,17 @@
 package com.tencent.wxcloudrun.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
     private String articleId;
     private String title;
