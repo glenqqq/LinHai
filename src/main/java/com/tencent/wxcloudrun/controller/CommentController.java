817442c89ef95ebd05fc044d1993012b8cfccbe0
yuelun <yuelun@amazon.com>
2025-07-22 23:43:16 +0800

    fix import issue

--- src/main/java/com/tencent/wxcloudrun/controller/CommentController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CommentController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.ArrayList;
 import java.util.List;
